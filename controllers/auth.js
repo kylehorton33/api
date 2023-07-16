0a150f6b123231294dd4aca10ef447fabd4ab440
Kyle Horton <kyle@cplventures.com>
2023-07-16 09:58:54 -0400

    feat: :card_file_box: return all fields except password hash

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const pool = require("../db/config");
 
 const getUsers = async(req, res) => {
-    const users = await pool.query('SELECT * FROM users');;
+    const users = await pool.query('SELECT user_id, email, username, usertype, admintype FROM users');;
     res.json(users.rows);
 }
 
