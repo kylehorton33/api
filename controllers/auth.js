65b9e6716be465a3352e76b0ff41f1a4ef9bdb57
Kyle Horton <kyle@cplventures.com>
2023-07-16 10:01:16 -0400

    docs: :memo: explanation of controller functions

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const pool = require("../db/config");
 
+// return all users
 const getUsers = async(req, res) => {
     const users = await pool.query('SELECT user_id, email, username, usertype, admintype FROM users');;
     res.json(users.rows);
--- controllers/main.js
+++ controllers/main.js
@@ -1,3 +1,4 @@
+// a message at root ('/') to confirm that the server is running
 const welcomeMessage = (req, res) => {
     res.json({
         message: "This message is sent from ./controllers/main.js",
